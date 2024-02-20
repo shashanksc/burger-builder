9216d23c135acdb04a76e0c06519f744175fa5bc
shashanksc <fvyrtyler@gmail.com>
2024-02-20 09:28:35 +0530

    updated modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,7 @@ class BurgerBuilder extends React.Component {
         this.setState({purchasing:false});
     }
     purchaseContinueHandler=()=>{
+        this.setState({loading:true});
         const order ={
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -93,8 +94,12 @@ class BurgerBuilder extends React.Component {
             deliveryMethod : 'fastest'
         }
         axios.post('/orders.json', order)
-        .then(response=>console.log(response))
-        .catch(error=>console.log(error));
+        .then(response=>{
+            this.setState({loading: false, purchasing:false});
+        })
+        .catch(error=>{
+            this.setState({loading: false, purchasing:false});
+        });
     }
     render(){
         const disabledInfo ={
